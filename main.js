449197df457d3dc052950a1d3f9b3292ebb211db
MQuinteiroS <marcos.v.q.silveira@gmail.com>
2022-09-25 16:12:15 -0300

    added figures

--- main.js
+++ main.js
@@ -6,14 +6,14 @@ let axis = 0;
 var c = document.getElementById('myCanvas');
 var img = new Image();
 var ctx = c.getContext('2d');
-var lngSelct = 'Português';
-let magicNumb = document.getElementById('inputFile').offsetHeight + document.getElementById('navBar').offsetHeight + 20;
+var lngSelct = 'English';
+let magicNumb = document.getElementById('inputFile').offsetHeight + document.getElementById('navBar').offsetHeight + 30;
 
 var languageObj = {
     'Português':{
         'buttonStart':'Começar',
         'methodModalLabel':'Selecione Método',
-        'continuos': 'Linha Contínua',
+        'continuous': 'Linha Contínua',
         'intersection': 'Linha de Intersecção',
         'navbarTitle': '<i class="fa fa-line-chart" aria-hidden="true"></i> Visualizador de Gráficos',
         'buttonNewAxis': 'Adicionar Eixo',
@@ -25,7 +25,14 @@ var languageObj = {
         'valMaxBtnLabel': 'Posição Max',
         'close': 'Fechar',
         'save': 'Salvar',
+        'submitLine': 'Plotar',
         'languageSelect': 'Selecionar Idioma',
+        'directionLabel': 'Direção',
+        'up': 'Cima',
+        'down': 'Baixo',
+        'left': 'Esquerda',
+        'right': 'Direita',
+        'tooltipdirection': 'Para qual direção o eixo deve procurar o outro eixo para a intersecção',
         'tooltipName': 'Nome do eixo selecionado',
         'tooltipValMin': 'Valor minimo',
         'tooltipValMinBtn': 'Posição do valor minimo.\nAperta com o mouse no local desejado',
@@ -35,7 +42,7 @@ var languageObj = {
     'English':{
         'buttonStart':'Begin',
         'methodModalLabel':'Select Method',
-        'continuos': 'Continuous Line',
+        'continuous': 'Continuous Line',
         'intersection': 'Intersection',
         'navbarTitle': '<i class="fa fa-line-chart" aria-hidden="true"></i> Graph Viewer',
         'buttonNewAxis': 'Add Axis',
@@ -47,7 +54,14 @@ var languageObj = {
         'valMaxBtnLabel': 'Max Position',
         'close': 'Close',
         'save': 'Save',
+        'submitLine': 'Plot',
         'languageSelect': 'Language Select',
+        'directionLabel': 'Direction',
+        'up': 'Up',
+        'down': 'Down',
+        'left': 'Left',
+        'right': 'Right',
+        'tooltipdirection': 'To witch direction the axis should look for the other axis to intersect',
         'tooltipName': 'Name of the axis',
         'tooltipValMin': 'Minimal value of the axis',
         'tooltipValMinBtn': 'Position of the minimal value.\nClick with the mouse on the desired position',
@@ -55,6 +69,8 @@ var languageObj = {
         'tooltipValMaxBtn': 'Position of the maximum value.\nClick with the mouse on the desired position'
     }
 }
+
+changeLanguage(lngSelct);
 
 'use strict';
 
@@ -98,16 +114,21 @@ document.getElementById('valMaxBtn').addEventListener('click', async function(){
 
 document.getElementById('save').addEventListener('click', async function(){
 	let name = document.getElementById('titleAxis').value;
-	let color = (axis == 0) ? 'blue' : 'purple';
+	let color = (axis == 0) ? '#0F7DC2' : '#7B0099';
 	let minValue = document.getElementById('valMin').value;
 	let maxValue = document.getElementById('valMax').value;
+    let direction = '';
+    if (method == 'intersection'){
+        direction = document.getElementById('formControlDirection').value;
+    }
 	pos[axis] = {
 		name: name,
 		color: color,
 		minValue: minValue.replace(',','.'),
 		maxValue: maxValue.replace(',','.'),
 		posMin: currentPosMin,
-		posMax: currentPosMax
+		posMax: currentPosMax,
+        direction: direction
 	}
 	createAxis(currentPosMax[0], currentPosMax[1], color, 3);
 	createAxis(currentPosMin[0], currentPosMin[1], color, 3);
@@ -118,6 +139,7 @@ document.getElementById('save').addEventListener('click', async function(){
 	document.getElementById('valMaxVal').textContent = '';
 	$('#axisModal').modal('hide');
 	createInput();
+    changeLanguage(lngSelct);
 	axis += 1;
 });
 
@@ -131,33 +153,63 @@ document.addEventListener('click', function(event){
 document.querySelector('.form-control').addEventListener('click', function(event){
     let element = event.target;
     lngSelct = element.value;
-    let currentLanguage = languageObj[element.value];
-    Object.keys(currentLanguage).forEach(x => {
-        let el = document.getElementById(x);
-        if (x === 'navbarTitle'){
-            el.innerHTML = currentLanguage[x];
-        }
-        else if(x.startsWith('tooltip')){
-            el.title = currentLanguage[x];
-        }
-        else{
-            el.textContent = currentLanguage[x];
-        }
-    });
+    changeLanguage(lngSelct);
 });
 
 document.querySelectorAll('.method-btn').forEach(x => {
     x.addEventListener('click', function(event) {
         let buttonSubmit = `<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#axisModal" id="buttonNewAxis" style="margin: 10px;">
-                                Adicionar Eixo
+                                ${languageObj[lngSelct]['buttonNewAxis']}
                             </button>`;
         
         let element = event.target;
         method = element.id;
+        if (method == 'intersection'){
+            let tr = document.createElement('tr');
+            let label = document.createElement('th');
+            label.innerHTML = '<label for="#direction" id="directionLabel"></label>';
+            tr.appendChild(label);
+            let space = document.createElement('th');
+            space.innerHTML = '&emsp;';
+            tr.appendChild(space);
+            let form = document.createElement('th')
+            form.innerHTML = `<select class="form-control" id="formControlDirection">
+                                <option id="up"></option>
+                                <option id="down"></option>
+                                <option id="left"></option>
+                                <option id="right"></option>
+                            </select>`;
+            tr.appendChild(form);
+            let info = document.createElement('th');
+            info.innerHTML = '<i class="fa fa-info-circle" aria-hidden="true"></i>';
+            info.id = 'tooltipdirection';
+            info['data-toggle'] = 'tooltip';
+            info.title = '';
+            tr.appendChild(info);
+            document.getElementById('modalTable').appendChild(tr);
+        }
         $('#methodModal').modal('hide');
+        changeLanguage(lngSelct);
         document.getElementById('form').innerHTML = buttonSubmit;
     });
 });
+
+
+function changeLanguage(language){
+    let currentLanguage = languageObj[language];
+    Object.keys(currentLanguage).forEach(x => {
+        let el = document.getElementById(x);
+        if (el && x === 'navbarTitle'){
+            el.innerHTML = currentLanguage[x];
+        }
+        else if(el && x.startsWith('tooltip')){
+            el.title = currentLanguage[x];
+        }
+        else if(el){
+            el.textContent = currentLanguage[x];
+        }
+    });
+}
 
 function createAxis(posX, posY, color, size){
 	ctx.strokeStyle = color;
@@ -169,9 +221,7 @@ function createAxis(posX, posY, color, size){
 }
 
 function createInput(){
-	let buttonSubmit = `<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#axisModal" id="buttonNewAxis" style="margin: 10px;">
-                            Adicionar Eixo
-                        </button>`;
+	let buttonSubmit = `<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#axisModal" id="buttonNewAxis" style="margin: 10px;">${languageObj[lngSelct]['buttonNewAxis']}</button>`;
 	let outerDiv
 	if(axis == 1){
 		outerDiv = document.getElementById('form');
@@ -182,7 +232,6 @@ function createInput(){
 	let div = document.createElement('div');
 	let label = document.createElement('label');
 	let input = document.createElement('input');
-    let plotLabel = (lngSelct === 'English') ? 'Plot' : 'Plotar';
 	label.textContent = pos[axis].name;
 	label.style['font-weight'] = 'bold';
 	label.style['font-size'] = '25px';
@@ -197,8 +246,8 @@ function createInput(){
 		outerDiv.innerHTML = outerDiv.innerHTML.replace(buttonSubmit, '');
 		outerDiv.innerHTML += div.outerHTML;
 		outerDiv.innerHTML += `<button type="button" class="btn btn-primary" data-toggle="modal" id="submitLine" style="margin: 10px;">
-		${plotLabel}
-		</button>`;
+                                ${languageObj[lngSelct]['submitLine']}
+                                </button>`;
 	}
 	else{
 		outerDiv.innerHTML = div.outerHTML;
@@ -210,7 +259,7 @@ function createInput(){
 function drawLine(){
 	ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, ctx.canvas.height*img.width/img.height, ctx.canvas.height);
 	if(method === 'continuous'){
-        drawLineContinuos();
+        drawLinecontinuous();
     }else{
         drawLineIntersection();
     }
@@ -225,7 +274,8 @@ function getPerpOfLine(x1,y1,x2,y2){ // the two points can not be the same
     return [-ny, nx]; // return the normal  rotated 90 deg
 }
 
-function drawLineContinuos(){
+function drawLinecontinuous(){
+    console.log('aa')
     let currentPoss = [];
 	pos.forEach(x => {
 		let value = document.getElementById(x.name + 'InputLabel').value;
@@ -238,7 +288,7 @@ function drawLineContinuos(){
 		let currentPosY = constantY*value + x.posMax[1] - x.maxValue*constantY
 		currentPoss.push([currentPosX, currentPosY]);
 	})
-	ctx.strokeStyle = 'black';
+	ctx.strokeStyle = '#000';
     ctx.lineWidth = 3;
 	ctx.beginPath();
     ctx.moveTo(currentPoss[0][0], currentPoss[0][1]);
@@ -248,7 +298,8 @@ function drawLineContinuos(){
 	createAxis(currentPoss[1][0], currentPoss[1][1], pos[1].color, 5);
 }
 
-function drawLineIntersection(){''
+function drawLineIntersection(){
+    console.log('bb')
     let currentPoss = [];
     let endPoss = [];
     pos.forEach((x, i) => {
@@ -266,7 +317,7 @@ function drawLineIntersection(){''
         const len = 100000 / Math.hypot(vectorX, vectorY);
         vectorX *= len;
         vectorY *= len;
-        if(i === 0){
+        if(x.direction === languageObj[lngSelct]['up'] || x.direction === languageObj[lngSelct]['left']){
             endPoss.push([currentPosX - vectorX, currentPosY - vectorY]);
         }else{
             endPoss.push([currentPosX + vectorX, currentPosY + vectorY]);
@@ -279,7 +330,7 @@ function drawLineIntersection(){''
         currentPoss[1][0], currentPoss[1][1],
         endPoss[1][0], endPoss[1][1]);
 
-    ctx.strokeStyle = 'black';
+    ctx.strokeStyle = '#000';
     ctx.lineWidth = 3;
     ctx.beginPath();
     ctx.moveTo(currentPoss[0][0], currentPoss[0][1]);
@@ -287,7 +338,7 @@ function drawLineIntersection(){''
     ctx.moveTo(currentPoss[1][0], currentPoss[1][1]);
     ctx.lineTo(intersection.x, intersection.y);
     ctx.stroke();
-	createAxis(intersection.x, intersection.y, 'pink', 3);
+	createAxis(intersection.x, intersection.y, '#e06377', 3);
 	createAxis(currentPoss[0][0], currentPoss[0][1], pos[0].color, 5);
 	createAxis(currentPoss[1][0], currentPoss[1][1], pos[1].color, 5);
 }
@@ -325,4 +376,4 @@ function intersect(x1, y1, x2, y2, x3, y3, x4, y4) {
             x :x, 
             y: y
         }
-  }
+}
